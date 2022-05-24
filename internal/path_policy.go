c1703eee034ebff3d0ab96f8cb47d57ee1e1f759
shacharn <shachar.nissan@ibm.com>
2022-05-24 12:23:45 +0300

    vault upgrade read-only mode

--- internal/path_policy.go
+++ internal/path_policy.go
@@ -35,12 +35,12 @@ func (ob *OrdersBackend) pathSecretPolicies() []*framework.Path {
 	}
 	operations := map[logical.Operation]framework.OperationHandler{
 		logical.ReadOperation: &framework.PathOperation{
-			Callback:    ob.secretBackend.PathCallback(ob.secretBackend.GetPolicies, atReadSecretPolicies, false),
+			Callback:    ob.secretBackend.PathCallback(ob.secretBackend.GetPolicies, atReadSecretPolicies, true),
 			Summary:     policyReadOpSummary,
 			Description: policyReadOpDesc,
 		},
 		logical.UpdateOperation: &framework.PathOperation{
-			Callback:    ob.secretBackend.PathCallback(ob.secretBackend.UpdatePolicies, atUpdateSecretPolicies, true),
+			Callback:    ob.secretBackend.PathCallback(ob.secretBackend.UpdatePolicies, atUpdateSecretPolicies, false),
 			Summary:     policyUpdateOpSummary,
 			Description: policyUpdateOpDesc,
 		},
