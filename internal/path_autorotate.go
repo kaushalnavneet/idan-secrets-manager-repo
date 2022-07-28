7d0ac3c6b3057cf386f826d15576dbff41124f7e
Tatyana <tatyanab@il.ibm.com>
2022-07-28 13:28:10 +0300

    run auto-renew in background context

--- internal/path_autorotate.go
+++ internal/path_autorotate.go
@@ -24,12 +24,12 @@ func (ob *OrdersBackend) pathAutoRotate() []*framework.Path {
 
 func (ob *OrdersBackend) autoRotateCertificates(ctx context.Context, req *logical.Request, _ *framework.FieldData) (*logical.Response, error) {
 	common.Logger().Info("AUTO ROTATE CERTIFICATES started")
-	common.PerformOperationOnAllSecrets(ctx, req, ob.secretBackend.GetMetadataClient(), ob.secretBackend.GetPluginSecretType(), ob.GetSecretBackendHandler().(*OrdersHandler).rotateCertIfNeeded)
+	go common.PerformOperationOnAllSecrets(context.Background(), req, ob.secretBackend.GetMetadataClient(), ob.secretBackend.GetPluginSecretType(), ob.GetSecretBackendHandler().(*OrdersHandler).rotateCertIfNeeded)
 	return nil, nil
 }
 
 func (ob *OrdersBackend) autoRotateCleanup(ctx context.Context, req *logical.Request, _ *framework.FieldData) (*logical.Response, error) {
 	common.Logger().Info(" Final running of AUTO ROTATE CERTIFICATES for this day")
-	common.PerformOperationOnAllSecrets(ctx, req, ob.secretBackend.GetMetadataClient(), ob.secretBackend.GetPluginSecretType(), ob.GetSecretBackendHandler().(*OrdersHandler).cleanupAfterRotationCertIfNeeded)
+	go common.PerformOperationOnAllSecrets(context.Background(), req, ob.secretBackend.GetMetadataClient(), ob.secretBackend.GetPluginSecretType(), ob.GetSecretBackendHandler().(*OrdersHandler).cleanupAfterRotationCertIfNeeded)
 	return nil, nil
 }
