2ae1d192b27fc4fc7b4df37a1dd8a0d39bb5e356
Tatyana <tatyanab@il.ibm.com>
2022-08-11 09:48:19 +0300

    run 50 threads in parallel

--- internal/constants.go
+++ internal/constants.go
@@ -37,7 +37,7 @@ const (
 )
 
 const (
-	MaxWorkers                               = 1
+	MaxWorkers                               = 50
 	MaxCertRequest                           = 50
 	CertRequestTimeout         time.Duration = 60 * 20 //wait 20 minutes till fail order
 	RotateIfExpirationIsInDays               = 31
